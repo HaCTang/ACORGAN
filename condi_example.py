5eefd5b83168ad967facb900476486e98b564ebf
HaCTang <2316301466@qq.com>
2025-01-04 08:34:29 -0500

    new

--- condi_example.py
+++ condi_example.py
@@ -3,24 +3,26 @@ from organ import ORGAN
 
 model = ORGAN('cond_NAPro', 'mol_metrics', params={
     'CLASS_NUM': 2,
-    'PRETRAIN_DIS_EPOCHS': 1,
-    'PRETRAIN_GEN_EPOCHS': 200,
-    'MAX_LENGTH': 100,
-    'LAMBDA': 0.5,
+    'PRETRAIN_DIS_EPOCHS': 10,
+    'PRETRAIN_GEN_EPOCHS': 250,
+    "GEN_HIDDEN_DIM": 64,
+    'MAX_LENGTH': 80,
     'EPOCH_SAVES':5,
     "DIS_L2REG": 0.2, 
     "DIS_EMB_DIM": 32, 
-    "DIS_FILTER_SIZES": [1, 2, 3, 4, 5, 8, 10, 15], 
-    "DIS_NUM_FILTERS": [50, 50, 50, 50, 50, 50, 50, 75], 
+    "DIS_FILTER_SIZES": [1, 2, 3, 4, 5, 8, 10, 15, 20], 
+    "DIS_NUM_FILTERS": [50, 50, 50, 50, 50, 50, 50, 75, 100], 
     "DIS_DROPOUT": 0.75,
-    "DIS_EPOCHS": 2
+    "DIS_EPOCHS": 2,
+    'LAMBDA_C': 0.2,
+    "WGAN": True
 })
 
 model.load_training_set('./data/train_NAPro.csv')
 model.set_training_program(['diversity'], [2])
 model.load_metrics()
 
-model.load_prev_pretraining(ckpt='ckpt/cond_NAPro_pretrain_ckpt')
+# model.load_prev_pretraining(ckpt='ckpt/cond_NAPro_pretrain_ckpt')
 # model.organ_train(ckpt_dir='ckpt')
 # model.load_prev_training(ckpt='./checkpoints/cond_NAPro/cond_NAPro_1.ckpt')
 model.conditional_train(ckpt_dir='ckpt', gen_steps=30)
